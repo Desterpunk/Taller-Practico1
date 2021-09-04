3e5c6df151da397d2427d6d57e7b957bd5a10921
Jhon Serna <jhonalfaber@gmail.com>
2021-09-04 16:23:01 -0500

    Creacion de clase Electrodomestico

---
+++ src/main/java/com/taller/Electrodomestico.java
@@ -0,0 +1,70 @@
+package com.taller;
+
+public class Electrodomestico {
+    private String consumo;
+    private String procedencia;
+    private double precioConsumo;
+    private double precioProcendencia;
+    private double precioTotal;
+
+    public Electrodomestico(String consumo, String procedencia) {
+        this.consumo = consumo;
+        this.procedencia = procedencia;
+        setPrecioConsumo(consumo);
+        setPrecioProcendencia(procedencia);
+        setPrecioTotal();
+    }
+
+    public double getPrecioConsumo() {
+        return precioConsumo;
+    }
+
+    public double getPrecioProcendencia() {
+        return precioProcendencia;
+    }
+
+    public double getPrecioTotal() {
+        return precioTotal;
+    }
+
+    public void setPrecioConsumo(String consumo){
+        switch (consumo.toLowerCase()) {
+            case "a":{
+                this.precioConsumo = 450000;
+                break;
+            }
+            case "b":{
+                this.precioConsumo = 350000;
+                break;
+            }
+            case "c":{
+                this.precioConsumo = 250000;
+                break;
+            }
+            default:{
+                this.precioConsumo = 0;
+                break;
+            }
+        }
+    }
+    public void setPrecioProcendencia(String procedencia){
+        switch (procedencia.toLowerCase()) {
+            case "nacional":{
+                this.precioProcendencia = 250000;
+                break;
+            }
+            case "importado":{
+                this.precioProcendencia = 350000;
+                break;
+            }
+            default:{
+                this.precioProcendencia = 0;
+                break;
+            }
+        }
+    }
+    public void setPrecioTotal(){
+        precioTotal = precioConsumo + precioProcendencia;
+    }
+
+}
---
+++ src/main/java/com/taller/Main.java
@@ -0,0 +1,8 @@
+package com.taller;
+
+public class Main {
+    public static void main(String[] args) {
+        Electrodomestico electrodomestico1 = new Electrodomestico("a","imPOrTado");
+        System.out.println(electrodomestico1.getPrecioProcendencia());
+    }
+}
